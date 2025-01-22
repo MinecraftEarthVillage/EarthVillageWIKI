32f528732674a321c0cfc2374e091f48b5583025
战虎 <earthvillage.top@outlook.com>
2025-01-23 00:35:56 +0800

    大更新

--- docs/nav/data.ts
+++ docs/nav/data.ts
@@ -52,25 +52,31 @@ export const NAV_DATA: NavData[] = [
     title: '插件部分',
     items: [
       {
-        icon: 'https://qwerty.fe-mm.com/apple-touch-icon.png',
+        icon: 'https://www.emojiall.com/en/header-svg/%F0%9F%91%91.svg',
         title: '王国系统',
         desc: '为团体服务的领地保护与国战系统',
         link: '/article/王国',
       },
       {
-        icon: '/logo.png',
+        icon: 'https://crafthead.net/avatar/8667ba71b85a4004af54457a9734eed7',
+        title: '皮肤/昵称',
+        desc: '传自定义你的外观',
+        link: '/article/皮肤',
+      },
+      {
+        icon: 'https://www.spigotmc.org/data/resource_icons/11/11480.jpg',
         title: '个人领地',
         desc: '面向个人的领地保护系统',
         link: '/article/个人领地',
       },
       {
-        icon: '/logo.png',
+        icon: 'https://www.spigotmc.org/data/resource_icons/80/80910.jpg',
         title: '公路与自由载具',
         desc: '不受限于轨道和时刻表的车子',
         link: '/article/载具',
       },
       {
-        icon: '/logo.png',
+        icon: 'https://zh.minecraft.wiki/images/Repeating_Command_Block.gif',
         title: '创造模式与多模式',
         desc: '独家创造模式体验系统，随时切换游戏模式',
         link: '/article/创造模式体验系统',
@@ -81,31 +87,31 @@ export const NAV_DATA: NavData[] = [
     title: 'MOD部分',
     items: [
       {
-        icon: '/logo.png',
+        icon: 'https://i.mcmod.cn/class/cover/20210723/1626978462_14273_psym.jpg',
         title: 'MTR铁路',
         desc: '不只是轨道~',
         link: '/article/mtr',
       },
      {
-        icon: '/logo.png',
+        icon: 'https://i.mcmod.cn/class/cover/20210723/1626978462_14273_psym.jpg',
         title: 'MTR帮助',
         desc: '关于MTR（铁路部分）的常见问题文档',
         link: '/article/mtrquesten',
       },
       {
-        icon: '/logo.png',
+        icon: 'https://i.mcmod.cn/item/icon/128x128/60/608415.png',
         title: 'MTR-航海',
         desc: '不只是轨道~',
         link: '/article/mtr航海',
       },
       {
-        icon: '/logo.png',
+        icon: 'https://i.mcmod.cn/item/icon/128x128/67/678968.png',
         title: 'MTR-航空',
         desc: '不只是轨道~',
         link: '/article/mtr飞机',
       },
       {
-        icon: '/logo.png',
+        icon: 'https://cdn.modrinth.com/data/86xjpqqS/9f58c3977a8949f75300f065c9a63f34022c415a.jpeg',
         title: 'YSM模型',
         desc: '与众不同的4D皮肤',
         link: '/article/YSM',
