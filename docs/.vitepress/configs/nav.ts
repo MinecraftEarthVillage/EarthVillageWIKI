1baad48d6ea89773e336640e9638c11473d7bff6
☭战虎☭·BattleTiger <earthvillage.top@outlook.com>
2025-11-26 11:57:27 +0800

    ...

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -5,6 +5,6 @@ export const nav: DefaultTheme.Config['nav'] = [
   { text: '地球村主页', link: 'https://emc.mysxl.cn' },
   {
   text: '《名人堂》',
-    link: 'https://lezi.earthvillage.top',
+    link: '../fool',
   },
 ]
